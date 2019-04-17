7079416083e8fca2975ffd71c6d22bf16e4d14ab
Khalid Fituri <k.fituri@almanar.com.kw>
2019-04-17 09:18:33 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Khalid Fituri on 4/17/19.
+//  Copyright © 2019 Khalid Fituri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
